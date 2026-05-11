fd3ebdf47bafdf982b92535e4cf1d48e6d2dca80
sifoukhelif <sifkhelif@hotmail.com>
2026-05-11 03:51:37 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@
 import type { Metadata, Viewport } from 'next'
 import { headers } from 'next/headers'
 import { Playfair_Display, DM_Sans } from 'next/font/google'
-import { ThemeProvider } from '@/components/ThemeProvider'
-import { SupabaseProvider } from '@/components/SupabaseProvider'
-import { I18nProvider } from '@/components/I18nProvider'
+import { ThemeProvider } from '../components/ThemeProvider'
+import { SupabaseProvider } from '../components/SupabaseProvider'
+import { I18nProvider } from '../components/I18nProvider'
 import { Toaster } from '@/components/ui/Toaster'
 import '@/styles/globals.css'
 
