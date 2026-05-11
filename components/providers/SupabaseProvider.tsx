7a3afdbf582563227aaf9cd8f68875ec41e42075
sifoukhelif <sifkhelif@hotmail.com>
2026-05-11 05:56:37 +0100

    Refactor SupabaseProvider component structure

--- components/providers/SupabaseProvider.tsx
+++ components/providers/SupabaseProvider.tsx
@@ -1,6 +1,18 @@
 'use client'
+
 import { createContext, useContext } from 'react'
+
 const Context = createContext<any>(undefined)
-export default function SupabaseProvider({ children }: { children: React.ReactNode }) {
-  return <Context.Provider value={{}}>{children}</Context.Provider>
+
+export function SupabaseProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <Context.Provider value={{}}>
+      {children}
+    </Context.Provider>
+  )
+}
+
+export const useSupabase = () => {
+  const context = useContext(Context)
+  return context
 }
