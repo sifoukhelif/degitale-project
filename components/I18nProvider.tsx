7140747fd9c06ce9a28d58ac0212daddc714ce18
sifoukhelif <sifkhelif@hotmail.com>
2026-05-11 04:54:26 +0100

    Delete components/I18nProvider.tsx

--- components/I18nProvider.tsx
+++
@@ -1,6 +0,0 @@
-'use client'
-import { createContext } from 'react'
-const Context = createContext<any>(undefined)
-export default function I18nProvider({ children }: { children: React.ReactNode }) {
-  return <Context.Provider value={{}}>{children}</Context.Provider>
-}
